8e5d4aa42b48feec35d38342d725032c3212b8ec
marceloabf <celo.filho1@hotmail.com>
2025-07-09 20:32:20 -0300

    [IA-LiveCompanion] Refactor CreateRoom component to remove room fetching logic and simplify layout

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,32 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
-import { Link } from 'react-router-dom';
-
-type getRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { CreateRoomForm } from '@/components/create-room-form';
+import { RoomList } from '@/components/room-list';
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms');
-      const result: getRoomsAPIResponse = await response.json();
-
-      return result;
-    },
-  });
   return (
-    <div>
-      {isLoading && <p>Loading...</p>}
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return (
-            <Link className="text-lg" key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          );
-        })}
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
+          <RoomList />
+          <div />
+        </div>
       </div>
     </div>
   );
